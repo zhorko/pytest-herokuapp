2d2f966185cde826014f86a1f067a8a7e9c26f39
zhork9 <zhorazhuzha@gmail.com>
2024-07-08 16:53:20 +0200

    Reformatted with 'black'

--- pages/hero_checkbox_page.py
+++ pages/hero_checkbox_page.py
@@ -2,24 +2,27 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common import exceptions
 
+
 class HeroCheckPage:
 
     def __init__(self, driver):
-        self.driver = driver        
-            
+        self.driver = driver
+
     def __find_checkb(self, wait):
         # Click on first checkbox
         try:
             checkB = wait.until(
-            EC.visibility_of_all_elements_located((By.CSS_SELECTOR, "#checkboxes input"))
+                EC.visibility_of_all_elements_located(
+                    (By.CSS_SELECTOR, "#checkboxes input")
+                )
             )
         except exceptions.TimeoutException as e:
-            print('{0} >> {1}'.format('hero_checkbox', e))
+            print("{0} >> {1}".format("hero_checkbox", e))
 
         return checkB
-        
+
     def click_check_1(self, wait):
-        
+
         checkB = self.__find_checkb(wait)
 
         # Check whether first checkbox is unchecked and check it
@@ -27,9 +30,9 @@ class HeroCheckPage:
             checkB[0].click()
         else:
             pass
-        
+
         return checkB[0].is_selected()
-    
+
     def click_check_2(self, wait):
 
         checkB = self.__find_checkb(wait)
@@ -39,5 +42,5 @@ class HeroCheckPage:
             checkB[1].click()
         else:
             pass
-        
+
         return checkB[1].is_selected()
--- tests/test_checkboxes.py
+++ tests/test_checkboxes.py
@@ -1,17 +1,17 @@
-from selenium.webdriver.support.wait import WebDriverWait 
+from selenium.webdriver.support.wait import WebDriverWait
 
 from pages.hero_home_page import HeroHomePage
 from pages.hero_checkbox_page import HeroCheckPage
 
 
 def test_checkboxes(_browser):
-    
+
     URL = "https://the-internet.herokuapp.com/"
     wait = WebDriverWait(_browser, 10)
 
     _browser.maximize_window()
     _browser.get(URL)
-    
+
     # Initializations
     hero_home_page = HeroHomePage(_browser)
     hero_check_page = HeroCheckPage(_browser)
@@ -20,4 +20,3 @@ def test_checkboxes(_browser):
 
     assert hero_check_page.click_check_1(wait) is True
     assert hero_check_page.click_check_2(wait) is True
-
